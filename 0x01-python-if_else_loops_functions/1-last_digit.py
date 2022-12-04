f2755475039882829e96aab5890ba10dc89d83b0
Williams <akanniwilliams@gmail.com>
2022-12-04 23:07:51 +0100

    Fix output for -ve number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,11 +4,13 @@ number = random.randint(-10000, 10000)
 print(f"Last digit of {number} is ", end="")
 if number < 0:
     number *= -1
-last_num = number % 10
-print(f'{last_num}', end="")
-if last_num > 5:
-    print(" and is greater than 5")
-elif last_num == 0:
-    print(" and is 0")
+    print(f"{(number % 10) * -1} and is less than 6 and not 0")
 else:
-    print(" and is less than 6 and not 0")
+    last_num = number % 10
+    print(f'{last_num}', end="")
+    if last_num > 5:
+        print(" and is greater than 5")
+    elif last_num == 0:
+        print(" and is 0")
+    else:
+        print(" and is less than 6 and not 0")
