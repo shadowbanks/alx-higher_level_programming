b3b59968bc63da549b4b68e8143bea27d4833d88
Williams <akanniwilliams@gmail.com>
2022-12-06 15:06:48 +0100

    Delete element at a specific location without pop

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new_list = my_list[:]
-    my_list.clear()
-    if idx < 0 or idx >= len(new_list):
-        return new_list
-    for i in new_list:
-        if i == idx + 1:
-            pass
-        else:
-            my_list.append(i)
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    del my_list[idx]
     return my_list
