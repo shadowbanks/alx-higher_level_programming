50f50bc0923c46fd980a9b00883027e7b0a1331b
Williams <akanniwilliams@gmail.com>
2023-01-07 23:22:03 +0100

    Handle TypeError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@ def safe_print_integer(value):
     status = False
     try:
         num = int(value)
-    except ValueError:
+    except (ValueError, TypeError):
         status = False
         pass
     else:
