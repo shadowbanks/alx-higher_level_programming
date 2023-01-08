2a53b9604f60a52442117ae1da1a3da212eb1929
Williams <akanniwilliams@gmail.com>
2023-01-08 07:07:03 +0100

    Fix function argument

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,8 +4,8 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        result = fct(args[0], args[1])
-    
+        result = fct(*args)
+
     except (ZeroDivisionError, TypeError, IndexError) as ex:
         result = None
         print("Exception: {}".format(ex), file=sys.stderr)
