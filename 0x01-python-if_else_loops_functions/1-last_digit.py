3abb49f8bd61507c1d7e85694dd5528421b6abd4
Williams <akanniwilliams@gmail.com>
2023-06-07 01:45:35 +0100

    Fix incorrect output if number is less than 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 reminder = abs(number) % 10
+if (number < 0):
+    reminder = reminder * -1
 print(f"Last digit of {number} is {reminder}", end=" ")
 if ((reminder) == 0):
     print("and is 0")
