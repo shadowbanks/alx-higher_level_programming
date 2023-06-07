2e3f4512d977c3c0c7a4da1771aa72e91bd8a88c
Williams <akanniwilliams@gmail.com>
2023-06-07 01:36:28 +0100

    Compare the last digit of a number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+reminder = abs(number) % 10
+print(f"Last digit of {number} is {reminder}", end = " ")
+if ((reminder) == 0):
+    print("and is 0")
+elif ((reminder) < 6):
+    print("and is less than 6 and not 0")
+else:
+    print("and is greater than 5")
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -7,3 +7,6 @@ If/else, loopa, functions
 - 0-positive_or_negative.py - Determine if a number is positive or negative
 
 
+- 1-last_digit - Compare the last digit of a number
+
+
