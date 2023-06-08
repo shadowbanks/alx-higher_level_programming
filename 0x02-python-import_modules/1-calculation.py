df3a980ea478011b553cd2445cc69aedd0ad2059
Williams <akanniwilliams@gmail.com>
2023-06-08 22:25:38 +0100

    'Create simple calculator'

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, div, mul
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    add_r = add(10, 5)
+    sub_r = sub(10, 5)
+    div_r = div(10, 5)
+    mul_r = mul(10, 5)
+
+    print("{} + {} = {}".format(a, b, add_r))
+    print("{} - {} = {}".format(a, b, sub_r))
+    print("{} * {} = {}".format(a, b, mul_r))
+    print("{} / {} = {}".format(a, b, div_r))
