dcf86296cf716b4374e1a896f928d8ae6e767297
Williams <akanniwilliams@gmail.com>
2023-06-09 00:11:36 +0100

    'Refactor the code'

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,12 +3,8 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, div, mul
     a = 10
     b = 5
-    add_r = add(10, 5)
-    sub_r = sub(10, 5)
-    div_r = div(10, 5)
-    mul_r = mul(10, 5)
 
-    print("{} + {} = {}".format(a, b, add_r))
-    print("{} - {} = {}".format(a, b, sub_r))
-    print("{} * {} = {}".format(a, b, mul_r))
-    print("{} / {} = {}".format(a, b, div_r))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b))
