28cf9510501df6339894421573508f257216e936
Williams <akanniwilliams@gmail.com>
2023-06-09 01:04:35 +0100

    'Change i[:1] to [:2] to check for the first 2 chars'

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,7 +5,5 @@ if __name__ == "__main__":
     hidden = dir(hidden_4)
     hidden.sort()
     for i in hidden:
-        if i[:1] == "__":
-            pass
-        else:
+        if i[:2] != "__":
             print("{}".format(i))
