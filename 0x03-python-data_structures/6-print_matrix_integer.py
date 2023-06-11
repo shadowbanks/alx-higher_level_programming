31aca83eba1ff3c5a4c525322797a8f24b1ed2d7
Williams <akanniwilliams@gmail.com>
2023-06-11 14:42:06 +0100

    'Add ":d" to the format string'

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
 
     for i in matrix:
         for j in range(len(i)):
-            print("{}".format(i[j]), end="")
+            print("{:d}".format(i[j]), end="")
             if j != len(i) - 1:
                 print(end=", ")
         print()
