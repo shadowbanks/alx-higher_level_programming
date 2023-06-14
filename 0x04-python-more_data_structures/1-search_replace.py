32f29cb7f7265eaa8600360f3f06abc9e6e0afb1
Williams <akanniwilliams@gmail.com>
2023-06-14 23:09:47 +0100

    'Add and remove space were necessary'

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def search_replace(my_list: list, search: int, replace: list) -> list:
-    return [i if i != search else replace for i in  my_list]
+    return [i if i != search else replace for i in my_list]
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-def uniq_add(my_list=[])-> int:
+def uniq_add(my_list=[]) -> int:
     return sum(set([i for i in my_list]))
