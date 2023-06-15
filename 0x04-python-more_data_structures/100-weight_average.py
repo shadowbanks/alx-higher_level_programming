612094dfe7b55d9d292fd3cd13c787d85858adde
Williams <akanniwilliams@gmail.com>
2023-06-15 01:48:43 +0100

    'Remove some spaces'

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if my_list:
-        return sum([x[0] * x[1] for x in my_list]) / sum([x[1] for x in my_list])
+        return sum([x[0]*x[1] for x in my_list])/sum([x[1] for x in my_list])
     else:
         return 0
