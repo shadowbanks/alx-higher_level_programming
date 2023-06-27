1190bf68b399dc05e9d8381e7b2c3d23ba34b8e7
Williams <akanniwilliams@gmail.com>
2023-06-27 03:17:01 +0100

    'Print an integer saafely'

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """Print an integer safely"""
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return False
+    else:
+        return True
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -4,3 +4,6 @@ Python - Exceptions
 	 * 0-safe_print_list - Print a list safely
 
 
+	 * 1-safe_print_integer - Print an integer safely, return True if printed else return False
+
+
