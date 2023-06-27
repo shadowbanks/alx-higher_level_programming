e4e6ecb4a14cc607277102b40a6df41972139a07
Williams <akanniwilliams@gmail.com>
2023-06-27 03:11:42 +0100

    'Print a list safely'

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """Print a list safely"""
+
+    count = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+        except IndexError:
+            pass
+        else:
+            count += 1
+    print()
+    return count
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,6 @@
+Python - Exceptions
+
+
+	 * 0-safe_print_list - Print a list safely
+
+
