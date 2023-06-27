82e653f1ffb0f255f758eed46be47695fddfd7fc
Williams <akanniwilliams@gmail.com>
2023-06-27 08:10:11 +0100

    Fix syntax and edit Error message

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     """Print an integer safely"""
     try:
         print("{:d}".format(value))
-    except:
+    except Exception:
         return False
     return True
--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     """Print an integer safely"""
     try:
         print("{:d}".format(value))
-    except ValueError as er:
+    except (ValueError, TypeError) as er:
         print(f"Exception: {er}", file=sys.stderr)
         return False
     return True
