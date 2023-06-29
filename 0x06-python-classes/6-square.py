1eb11a3e0f40258e39104dd97cbfb311ac5d6221
Williams <akanniwilliams@gmail.com>
2023-06-29 01:44:58 +0100

    'Check if position value is less the 0'

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,8 +7,8 @@ class Square:
     """Create a Square blueprint"""
 
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
