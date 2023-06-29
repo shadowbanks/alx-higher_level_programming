e145aa98a2b6c9ca22cfd440b56acac62577e7f3
Williams <akanniwilliams@gmail.com>
2023-06-29 01:14:28 +0100

    Adjust the sqaure with specific coordinate

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""Create a Square class"""
+
+
+class Square:
+    """Create a Square blueprint"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        s = self.__size
+
+        if s == 0:
+            print()
+        else:
+            for x in range(self.__position[1]):
+                print()
+            for i in range(s):
+                print(" " * self.__position[0], end="")
+                for j in range(s):
+                    print("#", end="")
+                print()
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -19,3 +19,6 @@ Python - Classes and Objects
 	 * 5-square - Draw a square using "#"
 
 
+	 * 6-square - Adjust the sqaure with specific coordinate
+
+
