bcb2a3db8c99f5cdc498985ef1318743f361327f
Williams <akanniwilliams@gmail.com>
2023-06-29 01:37:50 +0100

    'Check if position value is less the 0'

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,6 +47,8 @@ class Square:
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int) or not isinstance(value[0], int):
-            raise TypeError("size must be an integer")
+        elif not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
