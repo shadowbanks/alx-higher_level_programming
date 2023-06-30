41db2e70e8d90a59095c43c8e9702c4cd0aef78f
GhostProTech <akanniwilliams@gmail.com>
2023-06-30 02:10:58 +0100

    'Create a singly linked list (completed)'

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -12,19 +12,23 @@ class Node:
     @property
     def data(self):
         return self.__data
+
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
+        self.__data = value
 
     @property
     def next_node(self):
         return self.__next_node
+
     @next_node.setter
     def next_node(self, value):
-        if value not None or not isinstance(value, Node):
+        if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
 
 class SinglyLinkedList:
     """Create the singlyLinkedList"""
@@ -32,5 +36,28 @@ class SinglyLinkedList:
         self.__head = None
 
     def sorted_insert(self, value):
+        """Insert a new node into a linked list in a sorted manner"""
+        new_node = Node(value)
+        if self.__head is None:
+            self.__head = new_node
+        else:
+            temp = self.__head
+            prev = None
+            while temp is not None and temp.data < value:
+                prev = temp
+                temp = temp.next_node
+            if prev is None:
+                new_node.next_node = self.__head
+                self.__head = new_node
+            else:
+                prev.next_node = new_node
+                new_node.next_node = temp
+
+    def __str__(self):
+        """Print linked list data line by line"""
         temp = self.__head
-        new_node
+        linked_list = ""
+        while temp:
+            linked_list += str(temp.data) + "\n"
+            temp = temp.next_node
+        return linked_list.strip()
