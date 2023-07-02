4bee2380af65d646e49251e1771a62df345aecd7
Williams <akanniwilliams@gmail.com>
2023-07-02 13:01:09 +0100

    'Adds two numbers'

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+A funtion that adds 2 integer numbers together
+and return the sum of the two numbers
+"""
+import doctest
+
+
+def add_integer(a, b=98):
+    """
+    Calculate sum of two numbers
+    Args:
+        a (int): First integer
+        b (int): second integer (optional)
+
+    Returns:
+        Sum of a and b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    doctest.testfile("./tests/0-add_integer.txt")
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,9 @@
+Python - Test-driven development
+
+
+------------------------------------------
+
+
+	 * 0-add_integer - The function confirms if input is an integer/ a float, then add the two numbers (one number can be by default it will add to 98)
+
+
