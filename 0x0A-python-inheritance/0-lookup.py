527a31a0e8418f822df37e876e7e722d54c3e74c
GhostProTech <akanniwilliams@gmail.com>
2023-07-11 02:21:13 +0100

    'Returns a list of available attributes and method of an object'

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module documentation
+"""
+
+
+def lookup(obj):
+    """
+    Return the list of available attributes and
+    methods of an object
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,9 @@
+Python - Inheritance
+
+
+---
+
+
+Returns a list of available attributes and method of an object
+
+
