755fe76623b8e25279cf34b21a96f70bafc2c22e
Williams <akanniwilliams@gmail.com>
2023-07-11 12:31:03 +0100

    Remove "hard-coded value" from the return statement of ```inherits_from```

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     Check if a ``obj`` is an instance of ``a_class``
     that inherited (directly or indirectly)
     """
-    return issubclass(type(obj), int) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) != a_class
