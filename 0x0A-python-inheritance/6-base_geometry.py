7cdae757aafd237a2b56881fb179c8f5d8dfc7e0
Williams <akanniwilliams@gmail.com>
2023-07-11 04:39:07 +0100

    Raise an exception for unimplemented method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module doc for ```5-base_geometry```
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class
+    """
+    def area(self):
+        """ Calcualte the area"""
+        raise Exception("area() is not implemented")
