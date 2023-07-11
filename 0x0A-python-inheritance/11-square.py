af440afe93df17111df01f16a380f2554b3ef2d1
Williams <akanniwilliams@gmail.com>
2023-07-11 06:00:11 +0100

    Implement Square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -36,3 +36,18 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """Return string representation of the rectangle"""
         return f"[Rectangle] {self.__width}/{self.__height}"
+
+class Square(BaseGeometry):
+    """
+    A Square class inherting from BaseGeometry
+    """
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        """Return string representation of the rectangle"""
+        return f"[Square] {self.__size}/{self.__size}"
