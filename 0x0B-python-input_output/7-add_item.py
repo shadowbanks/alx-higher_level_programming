d3ff58d1a791239fb305e64f181d277f48e31118
GhostProTech <akanniwilliams@gmail.com>
2023-07-12 03:32:35 +0100

    'Create a script that adds all arguments to a list and save to a file'

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module ```7-add_item``` documentation
+"""
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_item():
+    """ Add all arguments to a list and save in a file"""
+    n = len(sys.argv)
+    my_list = []
+
+    for i in range(1, n):
+        my_list.append(sys.argv[i])
+
+    try:
+        load = load_from_json_file("add_item.json")
+        save = load + my_list
+        save_to_json_file(save, "add_item.json")
+    except FileNotFoundError:
+        save_to_json_file(my_list, "add_item.json")
+#    print(my_list)
+
+def main():
+    add_item()
+
+main()
