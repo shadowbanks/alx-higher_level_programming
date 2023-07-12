f9a156fbc2b1d61f47b54c42c4cccf42d6628d23
GhostProTech <akanniwilliams@gmail.com>
2023-07-12 02:35:35 +0100

    'Convert an object to a JSON representation'

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module ```3-to_json_string``` documentation
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ Convert an object to JSON representation"""
+    return json.dumps(my_obj)
