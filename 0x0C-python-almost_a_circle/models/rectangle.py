bc48b34f34d96d80022d55df6929624873ee25a3
Williams <akanniwilliams@gmail.com>
2023-07-15 14:51:39 +0100

    'Add the "str" magic method to display the details of the rectangle'

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -63,3 +63,9 @@ class Rectangle(Base):
         """ Display the rectangle"""
         for i in range(self.__height):
             print(f"{self.__width * '#'}")
+
+    def __str__(self):
+        return (
+                f"[Rectangle] ({self.id}) {self.__x}/{self.__y} "
+                f"- {self.__width}/{self.__height}"
+                )
