19c2708e436ea1787a2876a0a2e7b35749a53e73
GhostProTech <akanniwilliams@gmail.com>
2023-08-16 03:09:45 +0100

    'Insert a new row to a table'

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Create a new table
 CREATE TABLE IF NOT EXISTS first_table(
-	id INT PRIMARY KEY,
+	id INT,
 	name VARCHAR(256)
 );
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Insert a new row to the `first_table` table
+INSERT INTO first_table (id, name) VALUES(
+	89,
+	"Best School"
+);
