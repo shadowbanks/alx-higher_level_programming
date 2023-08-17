c4c0e4b85ec370046930fdea0ff362fa982505fc
GhostProTech <akanniwilliams@gmail.com>
2023-08-17 04:49:27 +0100

    'List only shows with no genre'

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
 -- Display all show including the ones with no genre
 SELECT s.title, sg.genre_id
-FROM tv_shows s LEFT OUTER JOIN tv_show_genres sg
+FROM tv_shows s LEFT JOIN tv_show_genres sg
 	ON s.id = sg.show_id
+WHERE sg.genre_id IS NULL
 ORDER BY s.title, sg.genre_id;
