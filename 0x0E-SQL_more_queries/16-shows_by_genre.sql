c349c0cb7bc8013896dcf63d5a1c055d435eb687
GhostProTech <akanniwilliams@gmail.com>
2023-08-17 05:55:58 +0100

    'Fixed'

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- Display all genre and tv shows linked to them
 SELECT s.title, g.name
-FROM tv_genres g
-INNER JOIN tv_show_genres sg
-	ON g.id = sg.genre_id
-INNER JOIN tv_shows s
+FROM tv_shows s
+LEFT OUTER JOIN tv_show_genres sg
 	ON s.id = sg.show_id
+LEFT OUTER JOIN tv_genres g
+	ON g.id = sg.genre_id
 ORDER BY s.title, g.name;
