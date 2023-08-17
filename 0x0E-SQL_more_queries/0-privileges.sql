bc8d1c6ba4d3c03546fb6db86f108795f9bec9cd
GhostProTech <akanniwilliams@gmail.com>
2023-08-17 02:08:50 +0100

    'Create a new user with all privileges'

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Show the lists of privileges to MySQL users
 -- ``user_0d_1`` and ``user_0d_2``
-SHOW GRANTS FOR "user_od_1"@"localhost";
-SHOW GRANTS FOR "user_od_2"@"localhost";
+SHOW GRANTS FOR 'user_od_1'@'localhost';
+SHOW GRANTS FOR 'user_od_2'@'localhost';
