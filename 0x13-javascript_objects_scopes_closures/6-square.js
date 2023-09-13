dbf99f5cfb19054adb254308350be6c5385a3e08
GhostProTech <akanniwilliams@gmail.com>
2023-09-13 04:03:29 +0100

    'Add a new method charPrint(Print a square using specific char, default: 'X''

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint(c){
+    let printChar = 'X';
+    if (c){
+      printChar = c;
+    }
+    let temp = ''; let i = 0;
+    while (i++ < this.width) {
+      temp += printChar;
+    }
+    i = 0;
+    while (i++ < this.height) {
+      console.log(temp);
+    }
+  }
+}
+
+module.exports = Square;
