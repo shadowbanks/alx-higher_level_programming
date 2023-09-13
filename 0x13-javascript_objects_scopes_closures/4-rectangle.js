f409961e87ad55dabf7d743e9354268ea7bc5bf7
GhostProTech <akanniwilliams@gmail.com>
2023-09-13 03:44:59 +0100

    'Add two new methods rotate(rotate the rectangle), double(double the size of the rectangle)'

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print(){
+    let temp = "", i = 0;
+    while (i++ < this.width){
+      temp += "X";
+    }
+    i = 0;
+    while (i++ < this.height){
+      console.log(temp);
+    }
+  }
+  rotate(){
+    [this.width, this.height] = [this.height, this.width];
+  }
+  double(){
+    [this.width, this.height] = [this.width * 2, this.height * 2];
+  }
+}
+
+module.exports = Rectangle;
