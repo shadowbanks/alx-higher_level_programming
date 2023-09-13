48facc3558dc0eb53081c6eaf44c7dbe51311115
Williams <akanniwilliams@gmail.com>
2023-09-13 22:55:47 +0100

    Use a counter(i) instead of indexOf to fix error that can occur when there are elements with the same value

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const data = require('./100-data.js');
 if (data.list) {
-  let temp = [];
+  let temp = []; let i = 0;
   const arr = data.list;
-  temp = arr.map((x) => x * arr.indexOf(x));
+  temp = arr.map((x) => x * i++);
   console.log(arr);
   console.log(temp);
 }
