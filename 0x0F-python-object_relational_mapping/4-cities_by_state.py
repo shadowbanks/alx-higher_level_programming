ebdaa4854c1ed58bc49b09c28e088dd51be435ea
GhostProTech <akanniwilliams@gmail.com>
2023-09-16 16:25:04 +0100

    'More on MySQLdb (without ORM)'

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys
+import MySQLdb
+
+def main():
+    userName, userPwd, dbname = sys.argv[1:4]
+
+    db = MySQLdb.connect(host='localhost', user=userName, passwd=userPwd, db=dbname, port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT c.id, c.name, s.name"+
+            " FROM cities c INNER JOIN states s"+
+                " ON c.state_id = s.id"+
+           " ORDER BY id")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+if __name__ == "__main__":
+    main()
