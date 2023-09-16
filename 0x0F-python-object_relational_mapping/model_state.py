19e7642e77f77075ce38d36bb75108431d254723
GhostProTech <akanniwilliams@gmail.com>
2023-09-16 20:05:46 +0100

    'Connect to table via ORM'

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Sequence
+
+Base = declarative_base()
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
