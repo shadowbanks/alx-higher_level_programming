0f95def3e7121e836e77e45c3d33998232256c85
GhostProTech <akanniwilliams@gmail.com>
2023-09-17 13:45:36 +0100

    'Add Doc to model_(state, city)'

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+"""
+City Schema
+"""
 from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class City(Base):
+    """
+    Create City schema
+    """
+
     __tablename__ = "cities"
 
     id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
+"""
+State Schema
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Sequence
+
 
 Base = declarative_base()
 
 
 class State(Base):
+    """
+    Create State Schema
+    """
+
     __tablename__ = "states"
 
     id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
