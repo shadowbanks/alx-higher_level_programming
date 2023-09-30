e0cf97691286ed8c5a399e18a772589d6f577ffa
Shadowbanks <akanniwilliams@gmail.com>
2023-09-30 05:03:25 +0100

    Files added: Get the response header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module Documentation"""
+import requests
+import sys
+
+
+def main():
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
+
+
+if __name__ == '__main__':
+    main()
