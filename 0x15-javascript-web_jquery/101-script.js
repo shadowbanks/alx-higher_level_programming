16605c55c5b709751390128e64a6ca40787ff6f4
Shadowbanks <akanniwilliams@gmail.com>
2023-11-06 12:53:29 +0100

    'Implement Add, Remove and clear list on click'

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+    $("div#add_item").on("click", function(){
+        $("ul.my_list").append("<li>Item</li>");
+    });
+    $("div#remove_item").on("click", function(){
+        $("ul.my_list li:last-child").remove();
+    });
+    $("div#clear_list").on("click", function(){
+        $("ul.my_list").empty();
+    });
+})
